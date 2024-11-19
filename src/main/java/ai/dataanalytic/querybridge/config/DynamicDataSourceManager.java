68e7a192659f137da92ee7f264cc4709696b4ad9
Enoc.Velza(Fungicoder) <fungicoder@gmail.com>
2024-11-19 16:30:56 -0600

    clean unused imports

--- src/main/java/ai/dataanalytic/querybridge/config/DynamicDataSourceManager.java
+++ src/main/java/ai/dataanalytic/querybridge/config/DynamicDataSourceManager.java
@@ -5,7 +5,6 @@ import ai.dataanalytic.sharedlibrary.dto.DatabaseConnectionRequest;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
