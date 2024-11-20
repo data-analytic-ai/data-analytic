cec3355dad92d9a5fe5f9bc941b450dee2a95d33
Enoc.Velza(Fungicoder) <fungicoder@gmail.com>
2024-11-19 23:58:23 -0600

    clean code

--- src/main/java/ai/dataanalytic/DataAnalyticApplication.java
+++ src/main/java/ai/dataanalytic/DataAnalyticApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "ai.dataanalytic.querybridge.mongodb.repository")
 public class DataAnalyticApplication {
 
     public static void main(String[] args) {
