9b46b2993127df7b639fe2506c1cb1f6629d329a
Jocsan Mairena <jocsan.mairena@gmail.com>
2025-10-13 18:46:04 -0400

    feat: Add application startup logging using Lombok @Slf4j Annotated DataAnalyticApplication with Lombok’s @Slf4j to enable SLF4J-based logging. Added a log.info() statement in the main method to log a message when the application starts. Ensures visibility into application lifecycle events, aiding in monitoring and troubleshooting. Follows Spring Boot best practices for logging and leverages Lombok for cleaner code.

--- src/main/java/ai/dataanalytic/DataAnalyticApplication.java
+++ src/main/java/ai/dataanalytic/DataAnalyticApplication.java
@@ -1,13 +1,16 @@
 package ai.dataanalytic;
 
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class DataAnalyticApplication {
 
     public static void main(String[] args) {
+        log.info("Starting DataAnalyticApplication...");
         SpringApplication.run(DataAnalyticApplication.class, args);
+
     }
 }
